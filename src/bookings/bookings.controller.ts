06e24fab6eb8719483c707637532446f5e89063e
Вадим <vadim@MBP-Vadim.NEOTECH>
2025-11-10 14:26:40 +0500

    boilerplate part done 2

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { ReserveDto } from './dto/reserve.dto';
@@ -8,16 +9,20 @@ export class BookingsController {
 
   @Post('reserve')
   @HttpCode(HttpStatus.CREATED)
-  async reserve(@Body() dto: ReserveDto) {
-    const booking = await this.bookingsService.reserve(
-      dto.event_id,
-      dto.user_id,
-    );
-    return {
+  async reserve(@Body() dto: ReserveDto): Promise<{
+    id: number;
+    event_id: number;
+    user_id: string;
+    created_at: Date;
+  }> {
+    const { event_id, user_id } = dto;
+    const booking = await this.bookingsService.reserve(event_id, user_id);
+    const response = {
       id: booking.id,
       event_id: booking.eventId,
       user_id: booking.userId,
       created_at: booking.createdAt,
     };
+    return response;
   }
 }
